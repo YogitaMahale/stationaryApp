1169d9b1095f6dcbf51626fe41aa4333ddb6fd2c
yogita mahale <mahaleyogita233@gmail.com>
2020-03-07 16:20:38 +0530

    bank

---
+++ App_Code/Cls_bankmaster_b.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using DatabaseLayer;
+
+namespace BusinessLayer
+{
+    public class Cls_bankmaster_b
+    {
+        public Cls_bankmaster_b()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+        #region Public Methods
+
+        public DataTable SelectAll()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                Cls_bankmaster_db objCls_bankmaster_db = new Cls_bankmaster_db();
+                dt = objCls_bankmaster_db.SelectAll();
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                ErrHandler.writeError(ex.Message, ex.StackTrace);
+                return dt;
+            }
+        }
+
+        
+
+        public bankmaster SelectById(Int64 bankid)
+        {
+            bankmaster objbankmaster = new bankmaster();
+            try
+            {
+                Cls_bankmaster_db objCls_bankmaster_db = new Cls_bankmaster_db();
+
+                objbankmaster = objCls_bankmaster_db.SelectById(bankid);
+                return objbankmaster;
+            }
+            catch (Exception ex)
+            {
+                ErrHandler.writeError(ex.Message, ex.StackTrace);
+                return objbankmaster;
+            }
+        }
+
+        public Int64 Insert(bankmaster objbankmaster)
+        {
+            Int64 result = 0;
+            try
+            {
+                Cls_bankmaster_db objCls_bankmaster_db = new Cls_bankmaster_db();
+
+                result = Convert.ToInt64(objCls_bankmaster_db.Insert(objbankmaster));
+                return result;
+            }
+            catch (Exception ex)
+            {
+                ErrHandler.writeError(ex.Message, ex.StackTrace);
+                return result;
+            }
+        }
+
+        public Int64 Update(bankmaster objbankmaster)
+        {
+            Int64 result = 0;
+            try
+            {
+                Cls_bankmaster_db objCls_bankmaster_db = new Cls_bankmaster_db();
+
+                result = Convert.ToInt64(objCls_bankmaster_db.Update(objbankmaster));
+                return result;
+            }
+            catch (Exception ex)
+            {
+                ErrHandler.writeError(ex.Message, ex.StackTrace);
+                return result;
+            }
+        }
+
+        public bool Delete(Int32 bankid)
+        {
+            try
+            {
+                Cls_bankmaster_db objCls_bankmaster_db = new Cls_bankmaster_db();
+
+                if (objCls_bankmaster_db.Delete(bankid))
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public bool IsActive(Int32 bankid, bool isActive)
+        {
+            try
+            {
+                Cls_bankmaster_db objCls_bankmaster_db = new Cls_bankmaster_db();
+                if (objCls_bankmaster_db.IsActive(bankid, isActive))
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        #endregion
+
+
+    }
+    public class bankmaster
+    {
+        public bankmaster()
+        { }
+
+        #region Private Variables
+        private Int64 _id;
+        private String _bankname;        
+        private Boolean _isactive;
+        private Boolean _isdelete;
+         
+       
+
+
+        #endregion
+
+
+        #region Public Properties
+        public Int64 id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public String bankname
+        {
+            get { return _bankname; }
+            set { _bankname = value; }
+        }
+       
+        public Boolean isactive
+        {
+            get { return _isactive; }
+            set { _isactive = value; }
+        }
+        public Boolean isdelete
+        {
+            get { return _isdelete; }
+            set { _isdelete = value; }
+        }
+
+     
+        #endregion
+    }
+
+}
---
+++ App_Code/Cls_bankmaster_db.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using BusinessLayer;
+
+namespace DatabaseLayer
+{
+    public class Cls_bankmaster_db
+    {
+        SqlConnection ConnectionString = new SqlConnection();
+
+        public Cls_bankmaster_db()
+        {
+            string name = string.Empty;
+            string conname = string.Empty;
+            ConnectionStringSettingsCollection connections = ConfigurationManager.ConnectionStrings;
+            if (connections.Count != 0)
+            {
+                foreach (ConnectionStringSettings connection in connections)
+                {
+                    name = connection.Name;
+                }
+                conname = "" + name + "";
+            }
+            ConnectionString.ConnectionString = ConfigurationManager.ConnectionStrings[conname].ConnectionString;
+        }
+        #region Public Methods
+
+       
+        public DataTable SelectAll()
+        {
+            DataSet ds = new DataSet();
+            SqlDataAdapter da;
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "bankmaster_SelectAll";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = ConnectionString;
+                ConnectionString.Open();
+                da = new SqlDataAdapter(cmd);
+                da.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                ErrHandler.writeError(ex.Message, ex.StackTrace);
+                return null;
+            }
+            finally
+            {
+                ConnectionString.Close();
+            }
+            return ds.Tables[0];
+        }
+
+        public bankmaster SelectById(Int64 bankid)
+        {
+            SqlDataAdapter da;
+            DataSet ds = new DataSet();
+            bankmaster objbankmaster = new bankmaster();
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "bankmaster_SelectById";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = ConnectionString;
+                cmd.Parameters.AddWithValue("@id", bankid);
+                ConnectionString.Open();
+                da = new SqlDataAdapter(cmd);
+                da.Fill(ds);
+
+                if (ds != null)
+                {
+                    if (ds.Tables.Count > 0)
+                    {
+                        if (ds.Tables[0] != null)
+                        {
+                            if (ds.Tables[0].Rows.Count > 0)
+                            {
+                                {
+                                    objbankmaster.id = Convert.ToInt32(ds.Tables[0].Rows[0]["id"]);
+                                    objbankmaster.bankname = Convert.ToString(ds.Tables[0].Rows[0]["bankname"]);
+                                    
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrHandler.writeError(ex.Message, ex.StackTrace);
+                return null;
+            }
+            finally
+            {
+                ConnectionString.Close();
+            }
+            return objbankmaster;
+        }
+
+        public Int64 Insert(bankmaster objbankmaster)
+        {
+            Int64 result = 0;
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "bankmaster_Insert";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = ConnectionString;
+
+                SqlParameter param = new SqlParameter();
+                param.ParameterName = "@id";
+                param.Value = objbankmaster.id;
+                param.SqlDbType = SqlDbType.Int;
+                param.Direction = ParameterDirection.InputOutput;
+                cmd.Parameters.Add(param);
+                cmd.Parameters.AddWithValue("@bankname", objbankmaster.bankname);
+              
+
+
+                ConnectionString.Open();
+                cmd.ExecuteNonQuery();
+                result = Convert.ToInt64(param.Value);
+            }
+            catch (Exception ex)
+            {
+                ErrHandler.writeError(ex.Message, ex.StackTrace);
+                return result;
+            }
+            finally
+            {
+                ConnectionString.Close();
+            }
+            return result;
+        }
+
+        public Int64 Update(bankmaster objbankmaster)
+
+        {
+            Int64 result = 0;
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "bankmaster_Update";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = ConnectionString;
+
+                SqlParameter param = new SqlParameter();
+                param.ParameterName = "@id";
+                param.Value = objbankmaster.id;
+                param.SqlDbType = SqlDbType.Int;
+                param.Direction = ParameterDirection.InputOutput;
+                cmd.Parameters.Add(param);
+                cmd.Parameters.AddWithValue("@bankname", objbankmaster.bankname);
+               
+                ConnectionString.Open();
+                cmd.ExecuteNonQuery();
+                result = Convert.ToInt64(param.Value);
+            }
+            catch (Exception ex)
+            {
+                ErrHandler.writeError(ex.Message, ex.StackTrace);
+                return result;
+            }
+            finally
+            {
+                ConnectionString.Close();
+            }
+            return result;
+        }
+
+        public bool Delete(Int32 bankid)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "bankmaster_Delete";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = ConnectionString;
+
+                cmd.Parameters.AddWithValue("@id", bankid);
+
+                ConnectionString.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                ErrHandler.writeError(ex.Message, ex.StackTrace);
+                return false;
+            }
+            finally
+            {
+                ConnectionString.Close();
+            }
+            return true;
+        }
+
+        public bool IsActive(Int32 bankid, bool isactive)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "bankmaster_IsActive";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = ConnectionString;
+                cmd.Parameters.AddWithValue("@id", bankid);
+                cmd.Parameters.AddWithValue("@isactive", isactive);
+
+                ConnectionString.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                ErrHandler.writeError(ex.Message, ex.StackTrace);
+                return false;
+            }
+            finally
+            {
+                ConnectionString.Close();
+            }
+            return true;
+        }
+
+        #endregion
+
+
+    }
+
+}
